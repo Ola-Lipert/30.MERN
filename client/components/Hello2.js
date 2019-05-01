9b10a61cca550253f098317d25a1ffc443e8c076
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-05-01 23:09:30 +0200

    Create your own generator

---
+++ client/components/Hello2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Hello2(props, context) {
+  return (
+  );
+}
+
+Hello2.propTypes = {
+};
+
+export default Hello2;
---
+++ client/components/Hello4.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+let Hello4 = (props, context) ==> (
+);
+
+
+Hello4.propTypes = {
+};
+
+export default Hello4;
