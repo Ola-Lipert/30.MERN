caa275a3126499b0012192e940766963b0a17154
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-05-03 23:39:44 +0200

    Finish componet PostDetailPage

--- client/modules/Post/pages/PostDetailPage/PostDetailPage.js
+++ client/modules/Post/pages/PostDetailPage/PostDetailPage.js
@@ -2,32 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
+import { injectIntl, FormattedMessage } from 'react-intl';
 
 // Import Style
 import styles from '../../components/PostListItem/PostListItem.css';
 
 // Import Actions
-import { fetchPost } from '../../PostActions';
+import { fetchPost, editPostRequest } from '../../PostActions';
+import { toggleEditPost } from '../../../App/AppActions';
 
 // Import Selectors
 import { getPost } from '../../PostReducer';
+import { getShowEditPost } from '../../../App/AppReducer';
 
 export class PostDetailPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: this.props.post.name,
+      title: this.props.post.title,
+      content: this.props.post.content,
+    };
   }
 
+  handleInputChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleEditPost = () => {
+    this.props.toggleEditPost();
+    this.props.editPostRequest(this.state);
+  };
+
   render() {
-    const { props } = this;
-    return (<div>
-      <Helmet title={props.post.title} />
-      <div className={`${styles['single-post']} ${styles['post-detail']}`}>
-        <h3 className={styles['post-title']}> {props.post.title} </h3>
-        <p className={styles['author-name']}> <FormattedMessage id="by" /> {props.post.name}</p>
-        <p className={styles['post-desc']}> {props.post.content}</p>
+    return (
+      <div>
+        <Helmet title={this.props.post.title} />
+        <a className={styles['edit-post-button']} href="#" onClick={this.props.toggleEditPost}><FormattedMessage id="editPost" /></a>
+        {
+          this.props.showEditPost
+            ? this.renderPostForm()
+            : this.renderPost()
+        }
       </div>
-    </div>
     );
   }
 
@@ -54,6 +75,19 @@ export class PostDetailPage extends React.Component {
   };
 }
 
+function mapDispatchToProps(dispatch, props) {
+  return {
+    toggleEditPost: () => dispatch(toggleEditPost()),
+    editPostRequest: (post) => dispatch(editPostRequest(props.params.cuid, post)),
+  };
+}
+
+function mapStateToProps(state, props) {
+  return {
+    post: getPost(state, props.params.cuid),
+    showEditPost: getShowEditPost(state),
+  };
+}
 // Actions required to provide data for this component to render in server side.
 PostDetailPage.need = [params => {
   return fetchPost(params.cuid);
@@ -74,6 +108,16 @@ PostDetailPage.propTypes = {
     slug: PropTypes.string.isRequired,
     cuid: PropTypes.string.isRequired,
   }).isRequired,
+  intl: PropTypes.shape({
+    messages: PropTypes.shape({
+      authorName: PropTypes.string.isRequired,
+      postTitle: PropTypes.string.isRequired,
+      postContent: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  showEditPost: PropTypes.bool.isRequired,
+  toggleEditPost: PropTypes.func.isRequired,
+  editPostRequest: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps)(PostDetailPage);
+export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(PostDetailPage));
