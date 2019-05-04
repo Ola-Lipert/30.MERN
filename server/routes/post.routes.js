b85e4e413a6b244e6ab78a3f93beabf48a9243bd
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-05-05 00:30:31 +0200

    Add post edits on the server side

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -8,6 +8,9 @@ router.route('/posts').get(PostController.getPosts);
 // Get one post by cuid
 router.route('/posts/:cuid').get(PostController.getPost);
 
+// Update a post by cuid
+router.route('/posts/:cuid').put(PostController.editPost);
+
 // Add a new Post
 router.route('/posts').post(PostController.addPost);
 
